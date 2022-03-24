418d2250f3b9b9084fcb2e6bb4a6bbbb0cf4e8be
Cabuke <habacuque.gabriel@gmail.com>
2022-03-24 11:03:55 -0300

    Aula 05 NLW

--- lib/modules/barcode_scanner/bar_code_scanner_page.dart
+++ lib/modules/barcode_scanner/bar_code_scanner_page.dart
@@ -48,7 +48,7 @@ class _BarCodeScannerPageState extends State<BarCodeScannerPage> {
               builder: (_, status, __) {
                 if (status.showCamera) {
                   return Container(
-                    child: status.cameraController!.buildPreview(),
+                    child: controller.cameraController!.buildPreview(),
                   );
                 } else {
                   return Container();
@@ -91,7 +91,9 @@ class _BarCodeScannerPageState extends State<BarCodeScannerPage> {
               ),
               bottomNavigationBar: SetLabelButtons(
                 primaryLabel: "Inserir código do boleto",
-                primaryOnPressed: () {},
+                primaryOnPressed: () {
+                  Navigator.pushReplacementNamed(context, "insert_boleto");
+                },
                 secondaryLabel: "Adicionar da galeria",
                 secondaryOnPressed: () {},
               ),
@@ -107,7 +109,7 @@ class _BarCodeScannerPageState extends State<BarCodeScannerPage> {
                         "Tente escanear novamente ou digite o código do seu boleto.",
                     primaryLabel: "Escanear novamente",
                     primaryOnPressed: () {
-                      controller.getAvailableCameras();
+                      controller.scanWithCamera();
                     },
                     secondaryLabel: "Digitar código",
                     secondaryOnPressed: () {},
